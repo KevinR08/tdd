e3ade4998780fc9c7f6aa7c9c52c138776e20a0c
Marcelinne <73668598+marcelinnes@users.noreply.github.com>
2022-11-26 21:29:29 -0500

    Establecer las pruebas a realizar para posterior desarollo. Escribir Prueba que Falle 1 , escribir prueba que falle por 2 , escribir prueba que falle 3, escribir prueba 4 para que falle 4 Se agregan como comentarios para posterios documentación

---
+++ src/test/java/ec/edu/epn/Logica/AgendaTest.java
@@ -0,0 +1,33 @@
+package ec.edu.epn.Logica;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AgendaTest {
+
+// Funcionalidad a implementar: Busqueda de sala por ID  o tipo de Moción Ingresados por el usuario final
+
+// Prueba 1 : Ingresando Tipo de Mocion , se despliega toda la info de salas con esa mocion
+// Prueba 2:  Ingresando un ID conocido de Sala, se despegle toda la información de la Sala
+// Prueba 3: Usuario puede elegir si ver las salas solo en estado disponible, no disponible o ambas
+// Prueba 4 : Mostrar mensaje de rechazo cuando no se encuentra la sala
+
+
+
+    @Test
+    public void agendarSala() {
+    }
+
+    @Test
+    public void mostrarSalas() {
+    }
+
+    @Test
+    public void getContSalas() {
+    }
+
+    @Test
+    public void getListaSalas() {
+    }
+}
