4d48b88138cecd5cb1c74887308d4b9b23aca5e7
Marcelinne <73668598+marcelinnes@users.noreply.github.com>
2022-11-26 21:31:58 -0500

    División de tipos de Pruebas

--- src/test/java/ec/edu/epn/Logica/AgendaTest.java
+++ src/test/java/ec/edu/epn/Logica/AgendaTest.java
@@ -8,11 +8,14 @@ public class AgendaTest {
 
 // Funcionalidad a implementar: Busqueda de sala por ID  o tipo de Moción Ingresados por el usuario final
 
+    // Tests Units
+
 // Prueba 1 : Ingresando Tipo de Mocion , se despliega toda la info de salas con esa mocion
 // Prueba 2:  Ingresando un ID conocido de Sala, se despegle toda la información de la Sala
 // Prueba 3: Usuario puede elegir si ver las salas solo en estado disponible, no disponible o ambas
 // Prueba 4 : Mostrar mensaje de rechazo cuando no se encuentra la sala
 
+    // Test Parametrizados
 
 
     @Test
